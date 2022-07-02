2c67643028b9041aa43c0eac28aaaec1901e3e8f
arsibux <arsibux@gmail.com>
2022-07-02 17:00:28 +0500

    init setup

---
+++ src/main/java/com/crossoriginspringboot/CrossOriginSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.crossoriginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossOriginSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossOriginSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crossoriginspringboot/CrossOriginSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossoriginspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossOriginSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
